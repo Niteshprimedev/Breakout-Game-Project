6d75a26a8e6998bf02c20bc83624479cf7d013d3
Nitesh <niteshprimedev@gmail.com>
2025-01-24 21:27:07 +0530

    Creating and Styling Breakout Game Page

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+const rulesBtnEl = document.getElementById('rules-btn');
+const rulesDivEl = document.getElementById('rules');
+const closeBtnEl = document.getElementById('close-btn');
+const canvasEl = document.getElementById('canvas');
+
+// Rules Show Event Handlers
+rulesBtnEl.addEventListener('click', showRules);
+
+function showRules(){
+    rulesDivEl.classList.add('show');
+}
+
+// Rules Close Event Handlers
+closeBtnEl.addEventListener('click', closeRules);
+
+function closeRules(){
+    rulesDivEl.classList.remove('show');
+}
